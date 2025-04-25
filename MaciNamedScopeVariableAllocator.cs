66aa37d9b8e6bbb9ba746d5f4794def2e72286e6
Fazin85 <grahamderson1@gmail.com>
2025-04-25 01:17:14 -0400

    variable allocators

---
+++ MaciNamedScopeVariableAllocator.cs
@@ -0,0 +1,81 @@
+﻿namespace MaciScript
+{
+    public class MaciNamedScopeVariableAllocator
+    {
+        private readonly Dictionary<string, Scope> scopes = [];
+
+        private class Scope
+        {
+            public List<Variable> Variables { get; } = [];
+        }
+
+        private class Variable
+        {
+            public required string Name;
+            public required int Value;
+        }
+
+        public void AllocateScope(string name)
+        {
+            if (scopes.ContainsKey(name))
+            {
+                throw new Exception("Cannot have multiple scopes with the same name: " + name);
+            }
+
+            scopes[name] = new();
+        }
+
+        public void AllocateVariable(string scopeName, string variableName)
+        {
+            if (!scopes.TryGetValue(scopeName, out Scope? scope))
+            {
+                throw new Exception("No scope with name: " + scopeName);
+            }
+
+            if (scope.Variables.Any(x => x.Name == variableName))
+            {
+                throw new Exception("Cannot have multiple variables with the same name in a scope: " + variableName);
+            }
+
+            scope.Variables.Add
+            (
+                new()
+                {
+                    Name = variableName,
+                    Value = 0
+                }
+            );
+        }
+
+        public int GetVariable(string scopeName, string variableName)
+        {
+            if (!scopes.TryGetValue(scopeName, out Scope? scope))
+            {
+                throw new Exception("No scope with name: " + scopeName);
+            }
+            var variable = scope.Variables.Find(x => x.Name == variableName);
+
+            return variable == null ? throw new Exception("No variable with name: " + variableName + " in scope: " + scopeName) : variable.Value;
+        }
+
+        public void SetVariable(string scopeName, string variableName, int value)
+        {
+            if (!scopes.TryGetValue(scopeName, out Scope? scope))
+            {
+                throw new Exception("No scope with name: " + scopeName);
+            }
+            var variable = scope.Variables.Find(x => x.Name == variableName) ?? throw new Exception("No variable with name: " + variableName + " in scope: " + scopeName);
+            variable.Value = value;
+        }
+
+        public void FreeScope(string name)
+        {
+            if (!scopes.ContainsKey(name))
+            {
+                throw new Exception("No scope with name: " + name);
+            }
+
+            scopes.Remove(name);
+        }
+    }
+}
---
+++ MaciStackVariableAllocator.cs
@@ -0,0 +1,89 @@
+﻿namespace MaciScript
+{
+    public class MaciStackVariableAllocator
+    {
+        private readonly Stack<Scope> scopes = [];
+        private class Scope
+        {
+            public List<Variable> Variables { get; } = [];
+        }
+        private class Variable
+        {
+            public required string Name;
+            public required int Value;
+        }
+
+        public void PushScope()
+        {
+            scopes.Push(new());
+        }
+
+        public void AllocateVariable(string name)
+        {
+            if (scopes.Count == 0)
+            {
+                throw new Exception("Tried to allocate stack variable, but no scope currently exists");
+            }
+            Scope scope = scopes.Peek();
+            if (scope.Variables.Any(x => x.Name == name))
+            {
+                throw new Exception("Cannot have multiple variables with the same name in a scope: " + name);
+            }
+            scope.Variables.Add(
+                new()
+                {
+                    Name = name,
+                    Value = 0
+                }
+            );
+        }
+
+        public int GetVariable(string name)
+        {
+            if (scopes.Count == 0)
+            {
+                throw new Exception("Tried to get stack variable, but no scope currently exists");
+            }
+
+            foreach (var scope in scopes)
+            {
+                var variable = scope.Variables.Find(x => x.Name == name);
+                if (variable != null)
+                {
+                    return variable.Value;
+                }
+            }
+
+            throw new Exception($"No variable with name: {name} found in any accessible scope");
+        }
+
+        public void SetVariable(string name, int value)
+        {
+            if (scopes.Count == 0)
+            {
+                throw new Exception("Tried to set stack variable, but no scope currently exists");
+            }
+
+            foreach (var scope in scopes)
+            {
+                var variable = scope.Variables.Find(x => x.Name == name);
+                if (variable != null)
+                {
+                    variable.Value = value;
+                    return;
+                }
+            }
+
+            throw new Exception($"No variable with name: {name} found in any accessible scope");
+        }
+
+        public void PopScope()
+        {
+            if (scopes.Count == 0)
+            {
+                throw new Exception("Cannot pop scope, no scope currently exists");
+            }
+            scopes.Pop();
+        }
+    }
+}
